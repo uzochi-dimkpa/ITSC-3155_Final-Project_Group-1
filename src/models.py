0be0a21aab25ae45fe1a8937271bae90c76da837
jake-blackb135 <jake@choosecarolinas.com>
2022-04-08 12:18:18 -0400

    Comment class correction

--- src/models.py
+++ src/models.py
@@ -28,8 +28,8 @@ class Post(db.Model):
 class Comment(db.Model):
     # TODO: Update SQLAlchemy Table here
     comment_id = db.Column(db.Integer, primary_key=true)
-    post_id = db.Column(db.Integer, nullable=true)
-    user_id = db.Column(db.Integer, nullable=true)
+    post_id = db.Column(db.Integer, nullable=false)
+    user_id = db.Column(db.Integer, nullable=false)
     title = db.Colum(db.Text, nullable=false)
     comment_text = db.Column(db.Text, nullable=false)
     created_at = db.Column(db.DateTime, nullable=false)
@@ -38,14 +38,14 @@ class Comment(db.Model):
 
 
     def __repr__(self):
-        return f'Question class'
+        return f'Comment class'
 
 
 
 # Server-side resource #1
 class Language(db.Model):
     # TODO: Update SQLAlchemy Table here
-    language_name = db.Column(db.VarChar(255), nullable=false,unique=true,primary_key=true)
+    language_name = db.Column(db.String, nullable=false,unique=true,primary_key=true)
 
     def __repr__(self):
         return f'Language class'
@@ -57,7 +57,7 @@ language_singleton = Language()
 # Server-side resource #2
 class Tag(db.Model):
     # TODO: Update SQLAlchemy Table here
-    tag_name = db.Column(db.VarChar(255), nullable=true,unique=true,primary_key=true)
+    tag_name = db.Column(db.String, nullable=false,unique=true,primary_key=true)
 
     def __repr__(self):
         return f'Tag class'
