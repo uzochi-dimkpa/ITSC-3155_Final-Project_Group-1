9c6bccd9a61c2a8184ff6a7b40dcdd62ad49f62a
jake-blackb135 <jake@choosecarolinas.com>
2022-04-08 12:14:45 -0400

    backend changes jake

--- src/models.py
+++ src/models.py
@@ -27,6 +27,15 @@ class Post(db.Model):
 # CRUD Resource #3
 class Comment(db.Model):
     # TODO: Update SQLAlchemy Table here
+    comment_id = db.Column(db.Integer, primary_key=true)
+    post_id = db.Column(db.Integer, nullable=true)
+    user_id = db.Column(db.Integer, nullable=true)
+    title = db.Colum(db.Text, nullable=false)
+    comment_text = db.Column(db.Text, nullable=false)
+    created_at = db.Column(db.DateTime, nullable=false)
+    updated_at = db.Column(db.DateTime, nullable=true)
+
+
 
     def __repr__(self):
         return f'Question class'
@@ -36,6 +45,7 @@ class Comment(db.Model):
 # Server-side resource #1
 class Language(db.Model):
     # TODO: Update SQLAlchemy Table here
+    language_name = db.Column(db.VarChar(255), nullable=false,unique=true,primary_key=true)
 
     def __repr__(self):
         return f'Language class'
@@ -47,6 +57,7 @@ language_singleton = Language()
 # Server-side resource #2
 class Tag(db.Model):
     # TODO: Update SQLAlchemy Table here
+    tag_name = db.Column(db.VarChar(255), nullable=true,unique=true,primary_key=true)
 
     def __repr__(self):
         return f'Tag class'
