70708a63d1446dcd1ea279a8d39fcac638087dd5
Uzochi <uzochidimkpa@gmail.com>
2022-05-09 01:22:37 -0400

    BREAKING CHANGE: removed unnecessary comments

--- src/blueprints/post_blueprint.py
+++ src/blueprints/post_blueprint.py
@@ -125,16 +125,12 @@ def delete_post(post_id):
 
 @router.route('/<user_id>/<post_id>/comment', methods = ['GET', 'POST']) #- 
 def create_comment(user_id, post_id):
-    # TODO: PLACEHODER; the following code will be replaced with code for user login
-    # session querying and for returning the user object to the '_layout.html' page
     logged_in_user = User.query.get(user_id)
 
     user_post = Post.query.get_or_404(post_id)
     post_comments = Comment.query.filter(Comment.post_id == post_id).all()
     
-    # post_id = request.form.get('post_id', '')
     post_id = post_id
-    # user_id = request.form.get('user_id', '')
     comment_text = request.form.get('comment_text', '')
     created_at = func.now()
     updated_at = None
@@ -162,18 +158,11 @@ def get_update_comment_form(post_id, comment_id):
     text = Comment.query.get(comment_id).comment_text
     return render_template('update-comment.html', post_id = post_id, comment_text = text, comment_id = comment_id)
 
-@router.post("/<post_id>/<comment_id>/update") #- /<comment_id>/update #- /{{ comment_id }}/update
-def update_comment(post_id, comment_id): #- , comment_id
-    # updated_comment = update(Comment).where(Comment.comment_id==comment_id).values(comment_text=body)
-    # updated_comment = Comment(comment_text=comment_text, user_id = user_id, post_id = post_id, created_at = created_at, updated_at = updated_at)
-    # {% if new_comment.user_id == logged_in_user.user_id %}{% endif %}
-
+@router.post("/<post_id>/<comment_id>/update")
+def update_comment(post_id, comment_id):
     updated_comment_text = request.form.get('comment_text', '')
     user_id = User.query.get(Post.query.get(post_id).user_id).user_id
     title = Post.query.get(post_id).title; body = Post.query.get(post_id).body
-    # post_id = post_id
-    # created_at = Comment.query.get(comment_id).created_at
-    # updated_at = func.now()
 
     if updated_comment_text == '':
         print(f'\n\n\nuser_id: {user_id}\npost_id: {post_id}\ncomment_text: "{updated_comment_text}"\n\n\n')
@@ -190,15 +179,6 @@ def update_comment(post_id, comment_id): #- , comment_id
 
 @router.get('/<post_id>/<comment_id>/delete')
 def delete_comment(post_id, comment_id):
-    # deleted_comment = delete(Comment).where(Comment.comment_id==comment_id)
-    # <!-- <form id="new_user_comment" action="/post/{{ post.post_id }}/{{ new_comment.comment_id }}/delete" method="POST">
-    #     <button form="new_user_comment" type="submit" class="btn btn-danger">Delete</button>
-    # </form> -->
-    # <!-- <form id="all_comments" action="/post/{{ post.post_id }}/{{ comment.comment_id }}/delete" method="POST">
-    #         <button form="all_comments" type="submit" class="btn btn-danger">Delete</button>
-    #     </form> -->
-
-    
     comment_to_delete = Comment.query.get_or_404(comment_id)
     db.session.delete(comment_to_delete)
     db.session.commit()
--- src/blueprints/user_blueprint.py
+++ src/blueprints/user_blueprint.py
@@ -3,21 +3,8 @@ from src.models import User, Post, Relationship, db
 
 router = Blueprint('user_router', __name__, url_prefix='/user')
 
-# @router.get('')
-# def user_profile():
-#     # TEMPORARY
-#     user_id = 1
-#     user = User.query.get_or_404(user_id)
-#     return render_template('profile.html', user = user) #- , user = user_id, friends = all_friends
-
 @router.get('/<user_id>')
 def user_profile_from_id(user_id):
     user = User.query.get_or_404(user_id)
     user_posts = Post.query.filter(Post.user_id == user_id).all()
     return render_template('profile.html', user = user, posts = user_posts)
-
-# @router.get('/<user_id>') #- /{{ user.user_id }}
-# def user_profile(user_id):
-#     user = User.query.get_or_404(user_id)
-#     user_friends = Relationship.query.filter_by(user_id_one = user_id, relate_type = 'friends').all()
-#     return render_template('profile.html', user = user, user_friends = user_friends)
